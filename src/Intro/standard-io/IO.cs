cd16bfdff10164b9bd5543c833e6b8c4a85697e5
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-06-08 17:40:40 +0700

    AddIOExample

---
+++ src/Intro/standard-io/IO.cs
@@ -0,0 +1,16 @@
+using System;
+namespace ConsoleSamp
+{
+  class Classs1
+  {
+    static void Main(string[] args)
+    {
+      Console.Write("Standard I/O Sample");
+      Console.WriteLine("");
+      Console.WriteLine("= = = = = = = = ");
+      Console.WriteLine("Enter your name . . .");
+      string name = Console.ReadLine();
+      Console.WriteLine("Output: Your name is : " + name);
+    }
+  }
+}
